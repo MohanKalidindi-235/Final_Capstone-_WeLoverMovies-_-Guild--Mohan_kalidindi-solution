b9543e522e7d7da0c47e2709989e405d6560d2dd
mkalidindi <mkalidindi@indeed.com>
2024-02-06 20:39:09 -0600

    updated database url

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,12 @@ const path = require("path");
 
 require("dotenv").config();
 
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL = "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop"  } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -19,7 +19,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
