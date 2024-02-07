e4756d2e5ab5601cea2b19307e95c9cdfba0eecb
mkalidindi <mkalidindi@indeed.com>
2024-02-06 21:34:20 -0600

    changed back the database url

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,12 @@ const path = require("path");
 
 require("dotenv").config();
 
-const { DATABASE_URL = "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop"  } = process.env;
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop",
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -19,7 +19,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: "postgres://test_postgresql_develop_user:tZuJKSMAGMxrtE2JWMOGnsdD3DTZCjv0@dpg-cn192ef109ks73ce7rpg-a/test_postgresql_develop",
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
